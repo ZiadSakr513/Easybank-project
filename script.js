a58fb935da824309c0227dd79ca159202ffc8a22
ZiadSakr513 <153561598+ZiadSakr513@users.noreply.github.com>
2023-12-29 20:50:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("menu2").addEventListener("click",function(){
+    
+    if(document.getElementById("fixed").style.display==""){
+       document.getElementById("fixed").style.display="block";
+    
+    }
+    else{
+        document.getElementById("fixed").style.display="";
+    }
+    
+})
